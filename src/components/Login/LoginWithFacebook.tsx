1875ee21fbdcb6519e992c582dfb4e0552ff55ef
ahippoly <ahippoly@student.42.fr>
2024-05-22 17:12:51 +0200

    updated login with facebook compopnent

--- src/components/Login/LoginWithFacebook.tsx
+++ src/components/Login/LoginWithFacebook.tsx
@@ -6,8 +6,8 @@ function LoginWithFacebook() {
 
   return <LoginButton
     name="Facebook"
-    clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''}
-    oauthBaseUrl='https://accounts.google.com/o/oauth2/v2/auth'
+    clientId={process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID || ''}
+    oauthBaseUrl='https://www.facebook.com/v17.0/dialog/oauth'
     icon={<FacebookIcon />}
   >
   </LoginButton>;
