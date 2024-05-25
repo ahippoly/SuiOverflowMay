29ec34ee442c5ca0fac085fc833685df8eaf6abc
nada-r <nadarzer@gmail.com>
2024-05-25 16:20:59 +0200

    create the layout on page 1

--- StashWallet/src/app/page.tsx
+++ StashWallet/src/app/page.tsx
@@ -9,7 +9,10 @@
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography, Button } from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
+import { SiTwitch as TwitchIcon } from 'react-icons/si';
+import WalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
 import MainContainer from '@/components/MainContainer';
 
@@ -23,8 +26,8 @@ export default function HomePage() {
   return (
       <Box
         sx={{
-          backgroundColor: "#4158D0",
-          backgroundImage: "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
+          backgroundColor: "#eeeeee",
+          //backgroundImage: "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
@@ -32,18 +35,85 @@ export default function HomePage() {
           height: '100%',
         }}
       >
-        <MainContainer>
-          <Stack 
+        <Stack 
           direction="column" 
           spacing={2}
           justifyContent="center"
+          alignItems="center"
+          sx={{
+            height: '100%',
+          }}
+        >
+          <Typography variant="h1" align="center" sx={{ fontWeight: '900' }}>OWN</Typography>
+          <Typography variant="h1" align="center" sx={{ fontWeight: '900' }}>DIGITAL ASSETS</Typography>
+          <Typography variant="h1" align="center" sx={{ fontWeight: '900' }}>ON SUI</Typography>
+          
+          <Stack 
+            direction="column" 
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
             sx={{
-              height: '100%',
+              marginTop: 4,
             }}
           >
-            
+            <Button 
+              variant="contained" 
+              sx={{ 
+                width: '200px', 
+                height: '50px', 
+                backgroundColor: 'black', 
+                color: 'white', 
+                borderRadius: '25px', 
+                fontSize: '0.75rem',
+                '&:hover': {
+                  backgroundColor: 'white',
+                  color: 'black',
+                }
+              }} 
+              startIcon={<GoogleIcon />}
+            >
+              Sign In with Google
+            </Button>
+            <Button 
+              variant="contained" 
+              sx={{ 
+                width: '200px', 
+                height: '50px', 
+                backgroundColor: 'black', 
+                color: 'white', 
+                borderRadius: '25px', 
+                fontSize: '0.75rem',
+                '&:hover': {
+                  backgroundColor: 'white',
+                  color: 'black',
+                }
+              }} 
+              startIcon={<TwitchIcon />}
+            >
+              Sign In with Twitch
+            </Button>
+            <Button 
+              variant="contained" 
+              sx={{ 
+                width: '200px', 
+                height: '50px', 
+                backgroundColor: 'white', 
+                color: 'black', 
+                border: '2px solid black', 
+                borderRadius: '25px', 
+                fontSize: '0.75rem',
+                '&:hover': {
+                  backgroundColor: 'black',
+                  color: 'white',
+                }
+              }} 
+              startIcon={<WalletIcon />}
+            >
+              Connect Wallet
+            </Button>
           </Stack>
-        </MainContainer>
+        </Stack>
       </Box>
   );
 }
