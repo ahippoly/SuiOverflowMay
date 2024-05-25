16c1184c8e348a897e0b35c4b3ef681c57c7355d
nada-r <nadarzer@gmail.com>
2024-05-25 17:03:23 +0200

    add footer

--- StashWallet/src/app/page.tsx
+++ StashWallet/src/app/page.tsx
@@ -13,8 +13,39 @@ import { Box, Stack, Typography, Button } from '@mui/material';
 import GoogleIcon from '@mui/icons-material/Google';
 import { SiTwitch as TwitchIcon } from 'react-icons/si';
 import WalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import SvgIcon from '@mui/material/SvgIcon';
+import Link from '@mui/material/Link';
 
 import MainContainer from '@/components/MainContainer';
+
+function DoorClosedIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M18 20V6a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v14" />
+      <path d="M2 20h20" />
+      <path d="M14 12v.01" />
+    </SvgIcon>
+  );
+}
+
+function LinkIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+      <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+    </SvgIcon>
+  );
+}
+
+function LinkedinIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+      <rect width="4" height="12" x="2" y="9" />
+      <circle cx="4" cy="4" r="2" />
+    </SvgIcon>
+  );
+}
 
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
@@ -114,6 +145,36 @@ export default function HomePage() {
             </Button>
           </Stack>
         </Stack>
+        <Stack 
+            direction="row" 
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              marginTop: 4,
+            }}
+          >
+            <TwitchIcon style={{ fontSize: 24 }} />
+            <TwitchIcon style={{ fontSize: 24 }} />
+            <TwitchIcon style={{ fontSize: 24 }} />
+          </Stack>
+
+          <Stack 
+            direction="row" 
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              marginTop: 4,
+              fontSize: '0.875rem',
+            }}
+          >
+            <Typography>©2024 MYSTEN LABS. ALL RIGHTS RESERVED.</Typography>
+            <Link href="#" sx={{ textDecoration: 'none', color: 'inherit' }}>FAQ</Link>
+            <Link href="#" sx={{ textDecoration: 'none', color: 'inherit' }}>TERMS & CONDITIONS</Link>
+            <Link href="#" sx={{ textDecoration: 'none', color: 'inherit' }}>PRIVACY POLICY</Link>
+            <Link href="#" sx={{ textDecoration: 'none', color: 'inherit' }}>MANAGE COOKIES</Link>
+          </Stack>
       </Box>
   );
 }
