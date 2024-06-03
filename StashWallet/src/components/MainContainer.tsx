37caffc50193dd3b016d4631fb2b5bc1bd4e5816
ahippoly <ahippoly@student.42.fr>
2024-06-03 19:59:23 +0200

    added responsive

--- StashWallet/src/components/MainContainer.tsx
+++ StashWallet/src/components/MainContainer.tsx
@@ -1,18 +1,36 @@
 import { Container, Paper } from '@mui/material';
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 function MainContainer({ children }: { children: React.ReactNode }) {
+  const isMobile = !useMediaQuery((theme) => theme.breakpoints.up('sm'));
+
+  if (isMobile) {
+    return (
+      <Paper
+        elevation={0}
+        sx={{
+          p: 4,
+          height: '100%',
+          width: '100%',
+          backgroundColor: 'background.default',
+        }}
+      >
+        <>{children}</>
+      </Paper>
+    );
+  }
+
   return (
     <Container maxWidth='xs'>
       <Paper
         elevation={8}
         sx={{
           p: 4,
-          height: ' 680px',
+          height: '680px',
           width: '400px',
           backgroundColor: 'background.default',
         }}
       >
-        {children}
+        <>{children}</>
       </Paper>
     </Container>
   );
